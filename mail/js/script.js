7f5d84b0e356235545763f70a99e88ac4f9a39e6
EndriuK <endrit.kaskija@gmail.com>
2024-06-28 16:13:11 +0200

    [UPD] creazione arry per mail e "event" per btn

---
+++ mail/js/script.js
@@ -0,0 +1,9 @@
+const emails = ["random1@gmail.com", "random2@gmail.com", "random3@gmail.com"];
+
+
+const button = document.getElementById("serach");
+
+button.addEventListener("click", function () {
+
+    let foundMail = false;
+}
