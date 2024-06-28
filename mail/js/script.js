4b428e9df44829762751915b1f86a80e467a1cf2
EndriuK <endrit.kaskija@gmail.com>
2024-06-28 16:25:10 +0200

    [UPD] aggiunto flag e recuperato valore input

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,9 +1,14 @@
 const emails = ["random1@gmail.com", "random2@gmail.com", "random3@gmail.com"];
 
+// VARIABILE DI CONTROLLO (FLAG)
 const button = document.getElementById("serach");
 
 button.addEventListener("click", function () {
   let foundMail = false;
+
+  // RECUPERO VALORE INPUT
+  let mail = document.getElementById("mail").value;
+  console.log(mail);
 
   for (let i = 0; i < emails.length; i++) {
     if (emails[i] === "random1@gmail.com") {
