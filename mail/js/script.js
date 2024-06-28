a3e538baf61f097247fe235609373bb3b0c7ec18
EndriuK <endrit.kaskija@gmail.com>
2024-06-28 16:19:53 +0200

    [UPD] creato condizione for

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,9 +1,19 @@
 const emails = ["random1@gmail.com", "random2@gmail.com", "random3@gmail.com"];
 
-
 const button = document.getElementById("serach");
 
 button.addEventListener("click", function () {
+  let foundMail = false;
 
-    let foundMail = false;
-}
+  for (let i = 0; i < emails.length; i++) {
+    if (emails[i] === "random1@gmail.com") {
+      foundMail = true;
+    }
+  }
+
+  if (foundMail === true) {
+    console.log("Email trovata");
+  } else {
+    console.log("Email non trovata");
+  }
+});
