3a451316c2468e09dcde11f7edc62ca848a15eb3
EndriuK <endrit.kaskija@gmail.com>
2024-06-28 17:05:07 +0200

    [UPD] readme dadi

---
+++ dadi/README.md
@@ -0,0 +1,7 @@
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+1. creo arrey
+2. creo ciclo for per riempire arrey con math.random
+3. creo ciclo for per confrontare numeri array
--- mail/js/script.js
+++ mail/js/script.js
@@ -1,24 +1,27 @@
 const emails = ["random1@gmail.com", "random2@gmail.com", "random3@gmail.com"];
 
-// VARIABILE DI CONTROLLO (FLAG)
+// RECUPERO IL PULSANTE DAL DOM
 const button = document.getElementById("serach");
 
 button.addEventListener("click", function () {
+  // RECUPERO VALORE INSERITO NEL CAMPO INPUT
+  let mail = document.getElementById("mail").value.tolo;
+
+  // VARIABILE DI CONTROLLO (FLAG)
   let foundMail = false;
 
-  // RECUPERO VALORE INPUT
-  let mail = document.getElementById("mail").value;
-  console.log(mail);
-
+  // CICLO L'ARRAY E CONFRONTO L'ELEMENTO CHE STO CICLANDO CON LA PAROLA INSERITA DALLA FORM
   for (let i = 0; i < emails.length; i++) {
-    if (emails[i] === "random1@gmail.com") {
+    if (emails[i].toLowerCase === mail) {
+      // SE E' VERA LA CONDIZIONE, IMPOSTO LA VARIABILE DI CONTROLLO A TRUE
       foundMail = true;
     }
   }
 
+  // MOSTRO IL MESSAGGIO CORRISPONDENTE A SECONDA DEL VALORE DELLA VARIABILE DI CONTROLLO
   if (foundMail === true) {
-    console.log("Email trovata");
+    console.log(`${mail.toLowerCase} trovata`);
   } else {
-    console.log("Email non trovata");
+    console.log(`${mail.toLowerCase} non trovata`);
   }
 });
